9da8b76a52a73cb0e407a2f5f329c9fa11405759
lasse <77295879+lassejlv@users.noreply.github.com>
2024-10-24 11:12:36 +0200

    small change

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
-  component: () => <div>Hello from home!!!</div>,
+  component: () => <div>Hello from home!!!!!!</div>,
 })
